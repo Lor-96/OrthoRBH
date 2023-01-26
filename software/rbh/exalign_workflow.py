144e7e79b7176a56256b60d125e746140b6d06ce
Lor-96 <82108575+Lor-96@users.noreply.github.com>
2023-01-26 14:42:49 +0100

    Update exalign_workflow.py

--- software/rbh/exalign_workflow.py
+++ software/rbh/exalign_workflow.py
@@ -2,6 +2,7 @@ from software.library.exalign import exaligntab
 
 def exalign_pipeline(blast1,blast2,percentage,threshold):
     rbh=exaligntab(blast1).exalignrbh(blast2,percentage,threshold)
+    print("The number of the Best Reciprocal Hit between the 2 species is: "+str(len(rbh.keys())))
     exaligntab(blast1).pltexaligntop1(species = 'species1')
     exaligntab(blast2).pltexaligntop1(species = 'species2')
     return rbh
